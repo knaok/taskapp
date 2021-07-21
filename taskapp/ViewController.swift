b8f6223c5adbeef3711c9666ac81d23d5d5107c5
knaok <nfruitsugar.211@gmail.com>
2021-07-21 20:11:14 +0900

    Uploaded up to chapter 4

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    //method to return the number fo cell
+    func tableView(_ tableView: UITableView,numberOfRowsInSection section:Int) -> Int {
+        return 0
     }
 
-
+    // mothod to return contents of each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    // method to exceute when selecting each cell
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    // method to inform the cell can be deleted
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle{
+        return .delete
+    }
+    
+    //method called when touched delete button
+    func tableView(_ tableView: UITableView, commit editStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
+        
+    }
 }
-
